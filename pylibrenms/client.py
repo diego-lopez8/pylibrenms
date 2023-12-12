be18bc9282ba23e50ec0458276a7e91b8e24cb81
Diego Lopez <diego50421@gmail.com>
2023-12-12 15:42:52 -0500

    implemented search method

--- pylibrenms/client.py
+++ pylibrenms/client.py
@@ -6,30 +6,63 @@ import requests
 class Librenms:
 
     def __init__(self, base_url, api_key):
+        """
+        Creates a LibreNMS client. 
+
+        Parameters:
+            - base_url: URL of the LibreNMS instance to query
+            - api_key: API token used for authentication. 
+        """
         if not base_url.endswith('/'):
             base_url += '/'
         self.url = base_url + "api/v0/"
         self._api_key = api_key
         self._headers = {"X-Auth-Token": self._api_key}
 
-    def _get(self, route):
+    def _get(self, route, columns=None):
         """
         'GET" call to be used by all functions. 
         """
         endpoint = self.url + route
+        params = {}
+        # TODO: figure out if supporting strings or lists as columns is a good idea, just testing with lists now
+        if columns:
+            params['columns'] = ','.join(columns) if isinstance(columns, list) else columns
         try:
-            response = requests.get(endpoint, headers=self._headers)
+            response = requests.get(endpoint, headers=self._headers, params=params)
             return response.json()
         except requests.exceptions.RequestException as e:
             # TODO: fix up these exceptions
             print(f"Error occurred: {e}")
 
-    def get_all_ports(self, ):
-        ...
+    def get_all_ports(self, columns=None):
+        """
+        Get all ports on all devices.
 
-    def search_ports(self, ):
-        ...
-    
+        Parameters:
+            - columns: columns to filter on.
+        """
+        return self._get("ports", columns=columns)
+
+    def search_ports(self, field, search_string, columns=None):
+        """
+        Search for ports matching a specific query. 
+
+        Parameters:
+            - field: a list of fields to search. Can be ifAlias, ifDescr, ifName, or all.
+            - search_string: the string to search. 
+            - columns: columns to filter on. 
+        """
+
+        if field not in ["ifAlias", "ifDescr", "ifName", "all"]:
+            # TODO: implement exception
+            print("Error!")
+        if field == "all":
+            return self._get("ports/search/" + str(search_string), columns=columns)
+        else:
+            # fields is any of the remaining ifAlias, ifDescr, ifName
+            return self._get("ports/search/" + str(field) + "/" + str(search_string), columns=columns)
+
     def ports_with_associated_mac(self,):
         ...
 
@@ -41,7 +74,7 @@ class Librenms:
             - port_id : a port ID
         """
 
-        return self._get("ports/" + str(port_id))
+        return self._get("ports/" + str(port_id), columns=None)
     
     def get_port_ip_info(self,):
         ...
